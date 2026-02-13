a595394a1ec1fa104ccd57d9fc85ba99ae32d07f
mihirkulkarni1 <mihir.kulkarni@gmail.com>
2026-02-13 19:42:49 +0530

    Second try add GTM to Next.js layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
+      <Script id="gtm" strategy="afterInteractive">
+    {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-NV5NHQ6T');
+    `}
+    </Script>
+        <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
